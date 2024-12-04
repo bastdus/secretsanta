30db4a7cf0709cfadce399775066c8330169c252
Bastien Duseaux <bduseaux@deezer.com>
2024-12-04 11:13:35 +0100

    add docs

--- script/shuffle.js
+++ script/shuffle.js
@@ -3,12 +3,19 @@ import fs from "fs";
 // ==========================
 // DATA
 // ==========================
+/**
+ * List of participants.
+ * @type {string[]}
+ */
 const participants = ["Tom", "Georgina", "Fanny", "Gael", "Natacha", "Bastien"];
 
 // ==========================
 // PASSWORD LIST
 // ==========================
-// it's better to have more word on this list than participants to avoid participant to have the same password
+/**
+ * List of passwords. Needs to have at least as many or more passwords as participants.
+ * @type {string[]}
+ */
 const passwordList = [
   "noel",
   "cadeaux",
@@ -28,6 +35,11 @@ const passwordList = [
 // ==========================
 // SHUFFLE FUNCTION
 // ==========================
+/**
+ * Shuffles an array in place.
+ * @param {any[]} array - The array to shuffle.
+ * @returns {any[]} The shuffled array.
+ */
 const shuffleArray = (array) => {
   const shuffled = [...array];
   for (let i = shuffled.length - 1; i > 0; i--) {
@@ -37,6 +49,11 @@ const shuffleArray = (array) => {
   return shuffled;
 };
 
+/**
+ * Shifts the elements of an array by moving the last element to the first position.
+ * @param {any[]} array - The array to shift.
+ * @returns {any[]} The shifted array.
+ */
 const shiftArray = (array) => {
   const copyArray = [...array];
   const lastElement = copyArray.pop();
@@ -44,6 +61,11 @@ const shiftArray = (array) => {
   return copyArray;
 };
 
+/**
+ * Creates pairs of givers and receivers from a list of participants.
+ * @param {string[]} participantsList - The list of participants.
+ * @returns {{giver: string, receiver: string}[]} The list of pairs with givers and receivers.
+ */
 const createPairs = (participantsList) => {
   // create a copy shuffled from the participants array
   const shuffledArray = shuffleArray(participantsList);
@@ -62,6 +84,12 @@ const createPairs = (participantsList) => {
 // ==========================
 // OBFUSCATE FUNCTION
 // ==========================
+/**
+ * Obfuscates a string by encoding it with a simple Caesar cipher
+ * @param {string} str
+ * @returns {string}
+ * eg: obfuscateString("hello") => "107-104-109-109-114"
+ */
 const obfuscateString = (str) => {
   let encoded = "";
   for (let i = 0; i < str.length; i++) {
@@ -71,6 +99,12 @@ const obfuscateString = (str) => {
   return encoded.slice(0, -1); // Supprime le dernier tiret
 };
 
+/**
+ * Deobfuscates a string by decoding it with a simple Caesar cipher
+ * @param {string} encodedStr
+ * @returns {string}
+ * eg: deobfuscateString("107-104-109-109-114") => "hello"
+ */
 const deobfuscateString = (encodedStr) => {
   let decoded = "";
   const parts = encodedStr.split("-");
@@ -94,7 +128,7 @@ const init = () => {
   const pairs = createPairs(participants);
   const shuffledPasswords = shuffleArray(passwordList);
 
-  const friends = pairs.map(({ giver, receiver }, index) => {
+  const result = pairs.map(({ giver, receiver }, index) => {
     const password = shuffledPasswords[index] + new Date().getFullYear();
 
     return {
@@ -106,14 +140,14 @@ const init = () => {
 
   fs.writeFileSync(
     "./src/data/secretSanta.json",
-    JSON.stringify(friends, null, 2)
+    JSON.stringify(result, null, 2)
   );
 
   console.log(
     "Secret Santa pairs have been shuffled and written to ./src/data/secretSanta.json"
   );
 
-  friends.map(({ name, password }) => {
+  result.map(({ name, password }) => {
     console.log(
       `🫥 ${deobfuscateString(name)} \n🔐 ${deobfuscateString(password)} \n`
     );
